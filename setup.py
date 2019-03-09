c6e48eaeeb19300ce9d8580521313bd1c358c008
Devin Stevenson <dstevenson@lendingclub.com>
2019-03-09 12:16:14 -0800

    update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,26 @@ import purepyhive
 
 
 setup(
-    name="purepyhive",
+    name="pure-pyhive",
     version=purepyhive.__version__,
+    author='Devin Stevenson',
     description="Pure Sasl Transport Thrift Transport for PyHive",
+    long_description='',
+    license='GPLv3+',
     url="https://github.com/devinstevenson/pure-pyhive",
+    keywords='pyhive',
     packages=['purepyhive'],
     install_requires=['pyhive', 'thrift', 'pure-sasl'],
-    
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
 )
